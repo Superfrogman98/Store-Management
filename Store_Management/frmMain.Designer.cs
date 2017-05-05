088cd118dbd97139211a96b335c4ea2788fe7234
Superfrogman98 <frogman98m@aol.com>
2017-05-05 12:01:53 -0500

    Initial Publish to github, contains base database, Main form with datagrid for the inventory view, and control layout.

---
+++ Store_Management/frmMain.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Store_Management
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeDatabaseToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerPurchasesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.storePurchasesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabInventory = new System.Windows.Forms.TabPage();
+            this.dgvInventory = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.tabControlMain.SuspendLayout();
+            this.tabInventory.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.reportsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(602, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeDatabaseToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // changeDatabaseToolStripMenuItem
+            // 
+            this.changeDatabaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeDatabaseToolStripMenuItem1});
+            this.changeDatabaseToolStripMenuItem.Name = "changeDatabaseToolStripMenuItem";
+            this.changeDatabaseToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.changeDatabaseToolStripMenuItem.Text = "Database";
+            // 
+            // changeDatabaseToolStripMenuItem1
+            // 
+            this.changeDatabaseToolStripMenuItem1.Name = "changeDatabaseToolStripMenuItem1";
+            this.changeDatabaseToolStripMenuItem1.Size = new System.Drawing.Size(166, 22);
+            this.changeDatabaseToolStripMenuItem1.Text = "Change Database";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inventoryToolStripMenuItem,
+            this.customerPurchasesToolStripMenuItem,
+            this.storePurchasesToolStripMenuItem});
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // inventoryToolStripMenuItem
+            // 
+            this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
+            this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.inventoryToolStripMenuItem.Text = "Inventory";
+            // 
+            // customerPurchasesToolStripMenuItem
+            // 
+            this.customerPurchasesToolStripMenuItem.Name = "customerPurchasesToolStripMenuItem";
+            this.customerPurchasesToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.customerPurchasesToolStripMenuItem.Text = "Customer Purchases";
+            // 
+            // storePurchasesToolStripMenuItem
+            // 
+            this.storePurchasesToolStripMenuItem.Name = "storePurchasesToolStripMenuItem";
+            this.storePurchasesToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.storePurchasesToolStripMenuItem.Text = "Store Purchases";
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabInventory);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 27);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(602, 407);
+            this.tabControlMain.TabIndex = 1;
+            // 
+            // tabInventory
+            // 
+            this.tabInventory.Controls.Add(this.dgvInventory);
+            this.tabInventory.Location = new System.Drawing.Point(4, 22);
+            this.tabInventory.Name = "tabInventory";
+            this.tabInventory.Size = new System.Drawing.Size(594, 381);
+            this.tabInventory.TabIndex = 0;
+            this.tabInventory.Text = "Inventory";
+            this.tabInventory.UseVisualStyleBackColor = true;
+            // 
+            // dgvInventory
+            // 
+            this.dgvInventory.AllowUserToAddRows = false;
+            this.dgvInventory.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Silver;
+            this.dgvInventory.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvInventory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvInventory.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvInventory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInventory.Location = new System.Drawing.Point(0, 0);
+            this.dgvInventory.Name = "dgvInventory";
+            this.dgvInventory.ReadOnly = true;
+            this.dgvInventory.RowHeadersVisible = false;
+            this.dgvInventory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvInventory.Size = new System.Drawing.Size(594, 381);
+            this.dgvInventory.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 434);
+            this.Controls.Add(this.tabControlMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Store Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControlMain.ResumeLayout(false);
+            this.tabInventory.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeDatabaseToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem storePurchasesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerPurchasesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inventoryToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabInventory;
+        private System.Windows.Forms.DataGridView dgvInventory;
+    }
+}
+
---
+++ Store_Management/frmMain.cs
@@ -0,0 +1,71 @@
+﻿//Programmer: Max Buckel
+//Date: 2017-5-1
+//Function: Manage the Inventory of the store, customer and store purchases for an individual store
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace Store_Management
+{
+    public partial class frmMain : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        BindingSource storeBindingSource = new BindingSource();
+        public frmMain()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+Directory.GetCurrentDirectory()+"\\Store.accdb";
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=H:\2016 school year\Store_Management\Store_Management\bin\Release\Store.accdb";
+            fillInventoryTable();
+        }
+
+
+        //closes the form
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void fillInventoryTable()
+        {
+            try
+            {
+                connection.Open();
+                OleDbCommand getInventory = new OleDbCommand();
+                getInventory.Connection = connection;
+                getInventory.CommandText = "SELECT ProductName, UPC, Instock, SellPrice, BuyCost FROM Products ";
+                OleDbDataAdapter adap = new OleDbDataAdapter(getInventory);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                getInventory.Dispose();
+                connection.Close();
+                storeBindingSource.DataSource = ds.Tables[0].DefaultView;
+                dgvInventory.DataSource = storeBindingSource;
+                dgvInventory.Columns[0].HeaderText = "Product Name";//set column 1 
+                //Keep column two header as UPC
+                dgvInventory.Columns[2].HeaderText = "Amount Instock";//set column 3
+                dgvInventory.Columns[3].HeaderText = "Sell Price";//set column 4
+                dgvInventory.Columns[4].HeaderText = "Buy Cost";//set column 5
+
+                dgvInventory.Columns[3].DefaultCellStyle.Format = "C2";
+                dgvInventory.Columns[4].DefaultCellStyle.Format = "C2";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Inventory Table Could Not Be Filled" + ex);
+            }
+            
+        }
+
+    }
+}
